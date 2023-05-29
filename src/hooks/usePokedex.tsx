5c4e66571fcbd4831d5e038f9dfa70e0ed225063
Sean O'Connor <so@seano.dev>
2023-05-29 09:23:59 -0600

    Update hook `usePokedex`
    Add `perPage` property to define the number of results per page

--- src/hooks/usePokedex.tsx
+++ src/hooks/usePokedex.tsx
@@ -13,18 +13,21 @@ export type PokedexData = {
 
 export default function usePokedex(
   dex: number = 1,
-  page: number = 1
+  page: number = 1,
+  perPage: number = 20
 ): PokedexData {
   const { data, error, isLoading } = useSWR<Pokedex, Error>(
     `pokedex/${dex}`,
     fetcher<Pokedex>
   );
 
-  const startIndex = (page - 1) * 20;
-  const endIndex = startIndex + 20;
+  const startIndex = (page - 1) * perPage;
+  const endIndex = startIndex + perPage;
   const slicedData = data?.pokemon_entries.slice(startIndex, endIndex);
 
-  const totalPages = !data ? 0 : Math.ceil(data.pokemon_entries.length / 20);
+  const totalPages = !data
+    ? 0
+    : Math.ceil(data.pokemon_entries.length / perPage);
 
   return {
     data: slicedData,
