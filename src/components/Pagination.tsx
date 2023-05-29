b1b951eee460fe8b4d8f32de1614f817a02d937d
Sean O'Connor <so@seano.dev>
2023-05-29 15:37:34 -0600

    Update Pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -37,7 +37,7 @@ export default function Pagination({
     <div className="inline-flex flex-col gap-2">
       {showPageCount && (
         <p className="text-center text-sm font-medium">
-          {currentPage} of {maxPage}
+          Page {currentPage} of {maxPage}
         </p>
       )}
       <div className="inline-flex items-center justify-center gap-2">
@@ -49,9 +49,9 @@ export default function Pagination({
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            strokeWidth={1.5}
+            strokeWidth={2}
             stroke="currentColor"
-            className="h-6 w-6">
+            className="h-4 w-4">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
@@ -67,9 +67,9 @@ export default function Pagination({
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            strokeWidth={1.5}
+            strokeWidth={2}
             stroke="currentColor"
-            className="h-6 w-6">
+            className="h-4 w-4">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
@@ -85,9 +85,9 @@ export default function Pagination({
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            strokeWidth={1.5}
+            strokeWidth={2}
             stroke="currentColor"
-            className="h-6 w-6">
+            className="h-4 w-4">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
@@ -103,9 +103,9 @@ export default function Pagination({
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            strokeWidth={1.5}
+            strokeWidth={2}
             stroke="currentColor"
-            className="h-6 w-6">
+            className="h-4 w-4">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
